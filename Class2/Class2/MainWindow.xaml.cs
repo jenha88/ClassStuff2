a7affce0d3f7d78bea19c08c8457327f031dcc2a
Jennifer Ha <Jennifer.Ha-1@ou.edu>
2021-09-14 16:05:53 -0500

    finish class stuff

---
+++ Class2/Class2/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Class2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Addbtb_Click(object sender, RoutedEventArgs e)
+        {
+            string id, firstname, lastname, probabtion, gpa, bursar;
+
+            id = IDbox.Text;
+            firstname = FBox.Text;
+            lastname = LBox.Text;
+            probabtion = ProbBox.Text;
+            gpa = GPAbox.Text;
+            bursar = BurBox.Text;
+
+            Student stud = new Student(Convert.ToInt32(id), firstname, lastname, Convert.ToDouble(bursar));
+            stud.GPA = Convert.ToDouble(gpa);
+
+            if (probabtion.ToLower()=="yes")
+            {
+                stud.IsOnProbation = true;
+            }
+            else
+            {
+                stud.IsOnProbation = false;
+
+
+            }
+            lstStud.Items.Add(stud);
+        }
+
+        private void lstStud_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Student selected = (Student) lstStud.SelectedItem;// () casting 
+            MessageBox.Show($"{ selected.ToString()} has a Bursar Balance of {selected.CheckBalance().ToString("C")}");
+        }
+    }
+}
---
+++ Class2/Class2/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    public class Student
+    {
+        public int SoonerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        private double BursarBalance; 
+
+        public Student() //Constructor 
+        {
+            SoonerID = 0;
+            FirstName = "";
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0;
+            BursarBalance = 0;
+
+        }
+        public Student(int id, string fName, string lName, double BursarBalance)//overload constructer
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            IsOnProbation = false;
+            GPA = 0;
+            this.BursarBalance = BursarBalance;
+        }
+        public void MakePayment(double amount )
+        {
+            if (amount<0)
+            {
+                Console.WriteLine("Amount must be postiive");
+            }
+
+            BursarBalance -= amount; //subtracting from the bursar account 
+        }
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+        public override string ToString()
+        {
+            return $"({LastName}, {FirstName} ({SoonerID.ToString("N0")})";
+        }
+    }
+   
+}
